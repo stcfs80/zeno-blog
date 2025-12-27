a91ccfcb12e2d2cb9643f2e0657b5e06bdbc11b6
zestcs <stcfs80@gmail.com>
2025-12-27 23:16:56 +0900

    Our first Cloud auto deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
                 <div>
                     <h1 class="text-3xl font-bold">Chirper에 오신 것을 환영합니다!</h1>
                     <p class="mt-4 text-base-content/60">새로운 Laravel 애플리케이션입니다. 이제 노래(또는 지저귐)하게 만들 시간입니다!</p>
+                    <p class="mt-2 text-sm text-gray-600">Now this is live on the internet! 🎉</p>
                 </div>
             </div>
         </div>
