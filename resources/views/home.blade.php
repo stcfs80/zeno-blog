9c611d2416acafa29dbe3bea535d0b2bd89c3d1c
zestcs <stcfs80@gmail.com>
2025-12-27 22:29:24 +0900

    test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        환영합니다
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Chirper에 오신 것을 환영합니다!</h1>
+                    <p class="mt-4 text-base-content/60">새로운 Laravel 애플리케이션입니다. 이제 노래(또는 지저귐)하게 만들 시간입니다!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
